f941e13d7e56aacf3b29b772be36382b3d17eaee
tuannn <nguyen.ngoc.tuan@framgia.com>
2013-10-31 17:04:48 +0700

    update localization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,14 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale
  
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    #cookies[:remember_locale] = I18n.locale
+    if(params[:locale])
+	  cookies.permanent[:remember_locale] = params[:locale] 
+	end   
+    I18n.locale = cookies[:remember_locale] || 	I18n.default_locale
+	#I18n.locale = params[:locale] || 	I18n.default_locale.to_s
   end
+  
   # Force signout to prevent CSRF attacks
   def handle_unverified_request
     sign_out
