a170902779f00572f4d73774b6354048c71a06f8
tuannn <nguyen.ngoc.tuan@framgia.com>
2013-10-31 15:13:02 +0700

    Localization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
 
+  before_filter :set_locale
+ 
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
   # Force signout to prevent CSRF attacks
   def handle_unverified_request
     sign_out
